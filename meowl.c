ff65cf5a7536e9f82b4297a973d1495fac515bb1
Frinz Sundiang <sundiangfrinz84@gmail.com>
2026-08-07 12:46:56 +0800

    initial setup

---
+++ meowl.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>  
+#include <stdbool.h> 
+#include <stddef.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+
+
+  return 0;
+}
